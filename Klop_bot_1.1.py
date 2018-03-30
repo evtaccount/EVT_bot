205d233a0d8a2fc23e54bdf0519911299670028e
Evgeny Evtushenko <4eshire.s@gmail.com>
2018-03-30 12:27:13 +0500

    Added webhook code

--- Klop_bot_1.1.py
+++ Klop_bot_1.1.py
@@ -4,8 +4,9 @@ from telebot import types
 import menus
 import functions
 import config
-import time
-import sys
+import os
+from flask import Flask, request
+import logging
 
 
 bot = telebot.TeleBot(config.API_TOKEN_EVT)
@@ -235,4 +236,25 @@ def choose_film(c):
     #     bot.send_message(c.from_user.id, str(key) + '. ' + film['film'] + ' - ' + str(film['vote_film']))
 
 
-bot.polling()
+
+# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
+
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://min-gallows.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
